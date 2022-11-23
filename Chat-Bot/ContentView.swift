4632268a441ab0a586adf9d859bb2317010146c3
davalcato <davalcato@yahoo.com>
2022-11-23 11:49:49 -0800

    added the Image(systemName: and the type field that says type something.

--- Chat-Bot/ContentView.swift
+++ Chat-Bot/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    // variables
+    @State private var messageText = ""
+    // array of messages of type string
+    @State var message: [String] = ["Welcome to Chat Bot"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                Text("iBot")
+                    .font(.largeTitle)
+                    .bold()
+                // add image
+                Image(systemName: "bubble.left.fill")
+                    .font(.system(size: 26))
+                // logo for app
+                    .foregroundColor(Color.blue)
+            }
+            // scrollview
+            ScrollView {
+                // add place holder for messages
+                
+                
+            }
+            // message bar
+            HStack {
+                // textfield
+                TextField("Type something", text: $messageText)
+            }
+        }
     }
 }
 
