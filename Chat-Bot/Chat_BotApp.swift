9771764fe1f65a11bddd6489be28e0a8cc0b56ce
davalcato <davalcato@yahoo.com>
2022-11-23 11:25:22 -0800

    first commit

---
+++ Chat-Bot/Chat_BotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_BotApp.swift
+//  Chat-Bot
+//
+//  Created by Daval Cato on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Chat_BotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
